174bdc6aabf8b71fa480b33f2f4f2799de1311ad
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-07 13:17:00 -0700

    printf code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - print a string to stdout
+ *
+ *
+ * Description: print a string using printf funtion
+ *
+ * Return: 0 to end program
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
