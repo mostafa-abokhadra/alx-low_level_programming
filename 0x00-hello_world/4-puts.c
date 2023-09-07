dfee0c5ad47f131f08b49bd11ae723444993692a
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-07 12:58:04 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - prints a string using puts 
+ *
+ * Return: 0 to end the program
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
