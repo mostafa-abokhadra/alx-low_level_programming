6ca5eaaa063bd2301b5bc54385fd880b7e699822
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-07 13:40:58 -0700

    sizeof code 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 
 /**
   * main - entry point
-  * 
+  *
   * Description: print the size of permitive data types in c languagh
-  * Return: 0 to end the program 
+  * Return: 0 to end the program
   *
   */
 int main(void)
@@ -13,8 +13,9 @@ int main(void)
 		"Size of an int: %d byte(s)\n"
 		"Size of a long int: %d byte(s)\n"
 		"Size of a long long int: %d byte(s)\n"
-		"Size of a float: %d byte(s)\n",
-		sizeof(char),sizeof(int),sizeof(long int),sizeof(long long int),sizeof(float));
+		"Size of a float: %d byte(s)\n"
+		, sizeof(char), sizeof(int), sizeof(long int)
+		, sizeof(long long int), sizeof(float));
 	return (0);
 }
 
