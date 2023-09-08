3b7a7a50bb895b3fc01c8b40902f9272fd841848
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-08 16:24:02 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: print base 16 hexadecimal nums
+  *
+  * Return: 0 to end the program
+  */
+
+int main(void)
+{
+		int i;
+
+		for (i = '0'; i<='9'; i++)
+			putchar(i);
+		for (i = 'a'; i <= 'f'; i++)
+			putchar(i);
+		putchar('\n');
+		return (0);
+}
