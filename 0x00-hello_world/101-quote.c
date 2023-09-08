7f34ac48317df298efcec88605b02951ed83cb0d
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-07 18:49:08 -0700

    printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  *
+  * description : print a string to stderr
+  * Return : always 1
+  */
+
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
