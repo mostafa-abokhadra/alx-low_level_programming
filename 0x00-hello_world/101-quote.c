2f337cadc9130af17fcde8f200a97388f5cebb05
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-07 19:04:09 -0700

    ok3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,14 +5,14 @@
   *
   * description : print a string to stderr
   *
-  * Return : always 1 to end the program 
+  * Return: always 1 to end the program
   * as int fuctioin should alway return an integer value
   */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\""
-			  " - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
