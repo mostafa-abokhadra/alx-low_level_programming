8c6fa0a09a1a252b56ce6152faa3f8ad5c53cab8
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-07 18:54:18 -0700

    pritn to stderror

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,13 @@
   * main - entry point
   *
   * description : print a string to stderr
-  * Return : always 1
+  * Return : always 1 to end the program
   */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\""
+			"- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
