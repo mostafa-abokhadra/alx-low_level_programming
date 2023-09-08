a651e9e4ac009ea77a3218d7725889f8f09ca016
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-08 16:37:20 -0700

    nums,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: printing base 10 numbers
+  * seperated by coma and space
+  *
+  * Return: 0 to end the program
+  */
+
+int main(void)
+{
+		int i;
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			putchar(i);
+			if (i != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
