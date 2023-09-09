4f926f3f20cfb7b0230491ca2f665111f1c4fbe8
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-08 17:26:02 -0700

    1-advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: printing to stdout
+  * all possible different combinations of two digits
+  *
+  * Return: 0 to end the program
+  */
+
+int main(void)
+{
+		int i,j;
+
+		for (i ='0' ; i <= '9'; i++)
+		{
+			for (j = i+1 ; j <= '9'; j++)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
