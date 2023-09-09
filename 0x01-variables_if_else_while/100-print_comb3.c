5724f1fe4e5806ddd028db96cc9565fd71ebb504
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-08 17:28:07 -0700

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@
 
 int main(void)
 {
-		int i,j;
+		int i, j;
 
-		for (i ='0' ; i <= '9'; i++)
+		for (i = '0' ; i <= '9'; i++)
 		{
-			for (j = i+1 ; j <= '9'; j++)
+			for (j = i + 1 ; j <= '9'; j++)
 			{
 				putchar(i);
 				putchar(j);
