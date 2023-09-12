ba6b8b4c5f3a4815a6f1b59775de9bb55abddecb
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-12 16:05:30 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_last_digit - printing last digit to stdout
+  * @num: the num to print it's last digit
+  *
+  * Description: returning a the remainder of a given number to 10
+  *
+  * Return: the remainder of the devesion of agiven num to 10
+  */
+int print_last_digit(int num)
+{
+		return (num%10);
+}
+
