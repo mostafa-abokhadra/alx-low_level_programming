fbaae6f6b3cf77fd92e9f72721bb2b234025e44a
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-12 14:14:44 -0700

    alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints alphabet
+  *
+  * Description: printing in lower case a to z
+  *
+  * Return: no return value to void function
+  */
+void print_alphabet(void)
+{
+		for(int i = 'a' ; i <= 'z' ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
