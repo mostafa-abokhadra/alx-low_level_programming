7efe63f2a6361fccc19fb0197dd4003c5ffcad57
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-12 14:53:48 -0700

    99

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,7 @@
 /**
   * int _islower - checks for lowercase character
   * @c: the character to be checked
-  *
-  *
-  * description: return 1 if a given char,
+  * Description:return 1 if a given char,
   * in the ascii code is from 97 to 122
   *
   * Return: 1 if lower case, 0 otherwise
