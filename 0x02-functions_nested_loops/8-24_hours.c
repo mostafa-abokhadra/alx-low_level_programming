f7dbebebd14b4386fc5aa8d303b85865ab168369
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-12 17:26:22 -0700

    nice

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,24 +9,24 @@
   */
 void jack_bauer(void)
 {
-		int i , j , k , l;
+		int i, j, k, l;
 
-		for ( i = 0 ; i <= 2 ; i++)
+		for (i = 0 ; i <= 2 ; i++)
 		{
-			for ( j = 0 ; j <= 9 ; j++)
+			for (j = 0 ; j <= 9 ; j++)
 			{
-				for( k = 0 ; k < 6 ; k++)
+				for(k = 0 ; k < 6 ; k++)
 				{
 					for(l = 0 ; l <= 9 ; l++)
 					{
-						_putchar('0'+i);
-						_putchar('0'+j);
+						_putchar('0' + i);
+						_putchar('0' + j);
 						_putchar(':');
-						_putchar('0'+k);
-						_putchar('0'+l);
+						_putchar('0' + k);
+						_putchar('0' + l);
 						_putchar('\n');
 						if (i == 2 && j == 3 && k == 5 && l == 9)
-							return ;
+							return;
 					}
 				}
 			}
