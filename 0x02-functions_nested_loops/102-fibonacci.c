3f68ef6ef6a128b049a659574510aa836676bebf
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-13 10:23:38 -0700

    fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+  * main - entry point
+  *
+  * Description: fibonacci 50  nums
+  *
+  * Return: nothing
+  */
+int main(void)
+{
+		unsigned long long int arr [50];
+		int i;
+
+		arr[0] = 1, arr[1] = 2;
+		for (i = 2 ; i < 50 ; i++)
+		{
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+		for (i = 0 ; i < 50 ; i++)
+		{
+			printf("%d, ", arr[i]);
+		}
+		printf("\n");
+}
