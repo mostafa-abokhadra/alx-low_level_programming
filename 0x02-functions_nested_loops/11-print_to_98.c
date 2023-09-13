90d04edd44fa39e62e2e84f6ce0f8dd28d0f85e3
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-13 09:54:05 -0700

    betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,57 +1,65 @@
 #include "main.h"
 
 /**
+  * print_single - print one digit nums
+  * @i: the one digit num
   *
+  * Description: printing a num using it's ascii value
   *
+  * Return: nothing
   */
-void print_single(int i )
+void print_single(int i)
 {
-if( i < 0 )
-{
-_putchar('-');
-_putchar('0' + (i-i-i) );
-}
-else
-{
-_putchar('0'+i);
-}
-_putchar(',');
-_putchar(' ');
-
+		if (i < 0)
+		{
+			_putchar('-');
+			_putchar('0' + (i - i - i));
+		}
+		else
+		{
+			_putchar('0' + i);
+		}
+		_putchar(',');
+		_putchar(' ');
 }
 
 /**
+  * convert - converting two or more digit nums to
+  * printable value
+  * @num: the number to be converted
   *
+  * Description: using the last digit method to
+  * find the ascii value
   *
-  *
+  * Return: nothing
   */
-void convert (int num)
+void convert(int num)
 {
-int arr [10]={0,0,0,0,0,0,0,0,0,0};
-int t = num, j = 0;
-if (num < 0){
-_putchar('-');
-t = num -num -num;
-}
-while(t)
-{
-arr[j] = t % 10;
-t /= 10;
-j++;
-}
-j--;
-for ( ; j >= 0 ; j--)
-{
-_putchar('0' + arr[j]);
-}
-if (num != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-j = 0;
-}
+		int arr [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, t = num, j = 0;
 
+		if (num < 0)
+		{
+			_putchar('-');
+			t = num -num -num;
+		}
+		while (t)
+		{
+			arr[j] = t % 10;
+			t /= 10;
+			j++;
+		}
+		j--;
+		for ( ; j >= 0 ; j--)
+		{
+			_putchar('0' + arr[j]);
+		}
+		if (num != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		j = 0;
+}
 
 /**
   * print_to_98 - prints nums to 98
@@ -63,25 +71,26 @@ j = 0;
   */
 void print_to_98(int n)
 {
-int i ;
-for(i = n ; i <= 98 ; i++)
-{
-if(i >= -9 && i <= 9)
-{
-print_single(i);
-}
-else
-{
-convert(i);
-}
-}
-if (n >98 )
-{
-for (i =n;i>=98;i--)
-{
-    convert (i);
-}
-}
-_putchar('\n');
+		int i;
+
+		for(i = n ; i <= 98 ; i++)
+		{
+			if(i >= -9 && i <= 9)
+			{
+				print_single(i);
+			}
+			else
+			{
+				convert(i);
+			}
+		}
+		if (n > 98)
+		{
+			for (i = n ; i>= 98 ; i--)
+			{
+				convert(i);
+			}
+		}
+		_putchar('\n');
 }
 
