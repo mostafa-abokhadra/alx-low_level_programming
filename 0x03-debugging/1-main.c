3453d18d57f22e94890282cd3f67a2c922fbc3d2
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 11:27:18 -0700

    ok5

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,9 +2,6 @@
 
 /**
 * main - causes an infinite loop
-*
-* Description: commenting code to stop infinity
-*
 * Return: 0
 */
 
@@ -15,16 +12,13 @@ int main(void)
 		printf("Infinite loop incoming :(\n");
 
 		i = 0;
-
 		/*
 		while (i < 10)
 		{
 			putchar(i);
 		}
 		*/
-
 		printf("Infinite loop avoided! \\o/\n");
 
 		return (0);
 }
-
