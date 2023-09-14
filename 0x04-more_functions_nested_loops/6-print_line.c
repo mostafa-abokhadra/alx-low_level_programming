ae7089b1f19dddb4950750c2c9430195a46b5561
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 16:28:01 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_line - printing to n number underscore
+  * @n: number of underscore to be printed
+  *
+  * Description: using putchar func to print undercore
+  *
+  * Return: nothing
+  */
+void print_line(int n)
+{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n')
+}
+
