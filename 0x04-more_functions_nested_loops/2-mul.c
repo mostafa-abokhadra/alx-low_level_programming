ab4e539977b2ec10379385b586ef09842dfa4932
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 14:44:38 -0700

    yes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * mul - add 2 nums
+  * @a: first num
+  * @b: second num
+  *
+  * Description: returning the sum of 2 integres
+  *
+  * Return: the result of adding process
+  */
+int mul(int a, int b)
+{
+	return (a + b);
+}
