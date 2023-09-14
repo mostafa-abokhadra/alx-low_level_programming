677fda88c328da48a3ccc7ea1525643982cdcdab
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 14:12:54 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+  * _isupprt - checking upper case letter
+  * @c: the letter to be checked
+  *
+  * Description: using ascii code to look for upper case letter
+  *
+  * Retrun: 1 if the letter is in upper case , 0 if otherwise
+  */
+int _isupper(int c)
+{
+		if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+
+		return (0);
+}
+
