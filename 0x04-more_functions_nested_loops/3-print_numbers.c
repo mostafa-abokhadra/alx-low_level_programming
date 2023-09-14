3eb56b5c86ff92f4fe0e276ce6f75d8042292f51
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 14:50:31 -0700

    try

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers - printing from 0 to 9
+  *
+  * Description: using putchar func to print the ascii value 
+  *
+  * Return: nothing
+  */
+void print_numbers(void)
+{
+		int i;
+
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+}
+
