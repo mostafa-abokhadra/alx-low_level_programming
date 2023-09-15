79c997bbe8b98787d8fb2ce2520f7768f63cd662
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 17:22:44 -0700

    ok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -25,7 +25,14 @@ int main(void)
 			}
 			else if (i % 5 == 0)
 			{
-				printf("Buzz ");
+				if (i == 100)
+				{
+					printf("Buzz");
+				}
+				else
+				{
+					printf("Buzz ");
+				}
 			}
 			else
 			{
