28e731bae80fec024361ef20405fc3e609bb886b
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 17:01:59 -0700

    ok

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
+  * print_square - printing square shape
+  * @size: the size of the square to be printed
   *
+  * Description: printing # as the square lines if size > 0
+  * new line if zero or less
   *
-  *
-  *
-  *
-  *
+  * Return: nothing
   */
 void print_square(int size)
 {
