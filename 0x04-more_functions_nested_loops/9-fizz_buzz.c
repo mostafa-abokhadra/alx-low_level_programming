e9a967edd85fa3345b18cc27dd0d509035f6a377
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-14 17:13:51 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  * main - printing from 1 to 100
+  *
+  * Description: if no remainder on division to 3 we priint fizz
+  * if on division on 5 we print buzz
+  * if on diviion on bothe we print fizzbuzz
+  *
+  * Return: nothing
+  */
+int main(void)
+{
+		int i;
+
+		for (int i = 1 ; i <= 100 ; i++)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+
+		return (0);
+}
+
