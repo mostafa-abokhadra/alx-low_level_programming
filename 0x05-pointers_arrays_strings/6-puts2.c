162332d7510b7452d9ff219a85cf1dace95ecf5a
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-18 06:31:11 -0700

    ok

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+  * get_length - sting length
+  * @s: the string
+  *
+  * Description: returning the length of a given string
+  *
+  * Return: the length
+  */
+int get_length(char *s)
+{
+		int i;
+
+		for (i = 0 ; s[i] != '\0' ; i++)
+		{
+		}
+
+		return (i);
+}
+
+/**
   * puts2 - print an odd string !
   * @str: the given string
   *
