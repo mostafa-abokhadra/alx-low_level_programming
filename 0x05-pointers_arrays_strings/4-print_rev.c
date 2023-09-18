43a0a5ca60915dc53e71eee7492c9cea21185c0f
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-17 23:18:53 -0700

    revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - printing a string in revers
+  * @s: the string to be reversed
+  *
+  * Description: getting the last element index the reversing back
+  *
+  * Return:nothing
+  */
+void print_rev(char *s)
+{
+		int i;
+
+		for (i = 0 ; s[i] != '\0' ; i++)
+		{
+		}
+		for (i ; i >= 0 ; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+}
+
