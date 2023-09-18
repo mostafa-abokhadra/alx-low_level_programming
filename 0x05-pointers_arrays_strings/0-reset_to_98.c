b5c3cf9e0b61c78fe56dbcb6836c082f8dd6b3e8
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-17 22:41:47 -0700

    files and 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - chang a variable to 98
+  * @*n: the value to be changed
+  *
+  * Description: changing by referece
+  *
+  * Return: nothing
+  */
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
+
