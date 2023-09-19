41c1dd6b0b509bfae58355a83a034795192958e6
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-19 00:28:23 -0700

    ok

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,27 +10,35 @@
   */
 int _atoi(char *s)
 {
-		int neg = 0, i, n = 0;
+		int pos = 1, i, n = 0;
 
 		for (i = 0 ; s[i] != '\0' ; i++)
 		{
+			if (s[i] == '-' || s[i] == '+')
+			{
+				if (pos == 1 && s[i] == '-')
+				{
+					pos = 0;
+				}
+				else if (pos == 0 && s[i] == '-')
+				{
+					pos = 1;
+				}
+			}
 			if (s[i] >= '0' && s[i] <= '9')
 			{
 				n *= 10;
-				if( s[i - 1] == '-')
-				{
-					neg = 1;
-				}
 				n += (s[i] - '0');
-
-				if (! (s[i + 1] >= '0' && s[i + 1] <= '9'))
+				if(!(s[i + 1] >= '0' && s[i + 1]<= '9'))
 				{
 					break;
 				}
 			}
 		}
-		if (neg)
+		if (!pos)
 		{
+			if(n < 0)
+				return (n);
 			n *= (-1);
 		}
 
