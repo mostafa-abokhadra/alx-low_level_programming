34fd2b66e6ce96962e4b000af36e8166267b3bc1
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-18 19:00:09 -0700

    oik

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strcpy - copying a string
+  * @dest: the destination to be copied at
+  * @src: the source of copying
+  *
+  * Description: copying a given string into another string
+  *
+  * Return: the destination string
+  */
+char *_strcpy(char *dest, char *src)
+{
+		int i;
+
+		for (i = 0 ; src[i] != '\0' ; i++)
+		{
+			dest[i] = src[i];
+		}
+
+		return (dest);
+}
+
