693f9ae8d2d88c98c5327dd1c4c0d3fd0197df07
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-25 14:33:57 -0700

    ok

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *
+  *
+  *
+  *
+  *
+  *
+  *
+  */
+char *_strpbrk(char *s, char *accept)
+{
+		int i, j;
+
+		for (i = 0 ; s[i] != '\0' ; i++)
+		{
+			for (j = 0; accept[j] != '\0' ; j++)
+			{
+				if (s[i] == accept[j])
+				{
+					return (&s[i]);
+				}
+
+			}
+		}
+
+		return (0);
+}
