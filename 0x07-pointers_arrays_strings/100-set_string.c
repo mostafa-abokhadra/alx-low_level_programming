3827ebffb99428a380cafec95cd0f63c98df3919
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-26 14:27:07 -0700

    easy beasy

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * set_string - pointer to pointer
+  * @s: the pointer to pointer
+  * @to: the pointer point to the needed string
+  *
+  * Description: conversion
+  * Return: nothing
+  */
+void set_string(char **s, char *to)
+{
+		*s = to;
+}
