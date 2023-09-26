c7826bb03f466696ad599b894abd162f11b64d91
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-26 14:46:26 -0700

    cheated 5-strstr cause of busy day :), will do it myself when i free tommorrow

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+  * compare - comparing 2 strings
+  * @s1: the first
+  * @s2: the second
+  *
+  * Description: any thing
+  * Return: 1 on success, 0 if not
+  */
+int compare(char *s1, char *s2)
+{
+		while (*s1 && *s2)
+		{
+			if (*s1 != *s2)
+			{
+				return (0);
+			}
+
+			s1++;
+			s2++;
+		}
+
+		return (*s2 == '\0');
+}
+
+/**
+  * _strstr - finding substr
+  * @haystack: the string
+  * @needle: the substr
+  *
+  * Description: anything
+  *
+  * Return: the substr if found, 0 if not
+  */
+char *_strstr(char *haystack, char *needle)
+{
+		while (*haystack != '\0')
+		{
+			if ((*haystack == *needle) && compare(haystack, needle))
+			{
+				return (haystack);
+			}
+			haystack++;
+		}
+
+		return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+int compare(char *s1, const char *s2);
 void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
