79071f502466381cc3496e31f067ef32b354d180
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-26 13:19:57 -0700

    ok

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_diagsums - sum of matrix diagnol
+  * @a: the matrix
+  * @size: the size
+  *
+  * Description: suming the 2 diagnols of a square matrix
+  *
+  * Return: nothing
+  */
+void print_diagsums(int *a, int size)
+{
+		int i, sum1 = 0, sum2 = 0, *ptr = a;
+
+		for (i = 0; i < size; i++, ptr += size)
+		{
+			sum1 += ptr[i];
+		}
+
+		ptr -= size;
+
+		for (i = 0; i < size; i++, ptr -= size)
+		{
+			sum2 += ptr[i];
+		}
+
+		printf("%d, %d\n", sum1, sum2);
+}
+
