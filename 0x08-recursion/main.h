f62e7c7c4d765045e14392be09dc259eb01ddb6d
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-28 10:52:51 -0700

    ok

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN-H
-#define MAIN-H
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,4 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-#endif /* MAIN-H */
+
+#endif /* MAIN_H */
