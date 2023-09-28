29f5ec48fab326a55b862b1a2541dae5517b0bdc
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-28 14:54:57 -0700

    ok

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - power of num
+  * @x: the num
+  * @y: the power
+  *
+  * Description: raising a given num to a given power
+  *
+  * Return: the result
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (_pow_recursion(x,y-1) * x);
+}
+
