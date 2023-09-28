f96037bf35d3b842d5bdd15bcc91db4002ec7f3c
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-28 11:00:47 -0700

    ok

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,14 @@
   *
   * Return: nothing
   */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 		if (*s == '\0')
 		{
 			return;
 		}
 
-		_puts_recursion(++s);
+		_print_rev_recursion(++s);
 		_putchar(*s);
 }
 
