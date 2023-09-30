697d79372bbe04af5c1a7990ddb97e8fb2fa52eb
Mostafa Bukhadra <mvxizk@gmail.com>
2023-09-30 12:04:22 -0700

    ok

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+  * sqrt2 - finding the square root
+  * @n: the number
+  * @num: squared number to check
+  *
+  * Description: find the square root of a given number
+  * Return: the square root
+  */
+int sqrt2(int n, int num)
+{
+		if ((num * num) > n)
+		{
+			return (-1);
+		}
+		if (n == (num * num))
+		{
+			return (num);
+		}
+		else
+		{
+			return (sqrt2(n, num + 1));
+		}
+}
+
+/**
+  * _sqrt_recursion - find a square root
+  * @n: the number to find it's square root
+  *
+  * Description: calling another function to do the procedure
+  *
+  * Return: the returned value form above function
+  *
+  */
+int _sqrt_recursion(int n)
+{
+		int num = 1;
+		return (sqrt2(n, num));
+}
+
