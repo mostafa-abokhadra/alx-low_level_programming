1d6dd4125c06ca2761e428ca58f1a4a6a17c7a80
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-02 09:42:49 -0700

    ok

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * @argc: count of passed arguments from shell
+  * @argv: list of arguments
+  *
+  * Description: printing the number of the arguments
+  * Return: zero to end
+  */
+int main(int argc, int __attruibute__((unused)) **argv)
+{
+		printf("%d\n", argc);
+		return (0);
+}
