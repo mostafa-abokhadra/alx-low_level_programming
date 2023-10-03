bed8dbfed618b4ff955514356b34b8dacd72cab0
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-02 18:17:50 -0700

    ok

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,59 +1,63 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-  * main - entry point
-  * @argc: arg count
-  * @argv: list of arg
-  *
-  * Description: sum of arguments
-  *
-  * Return: 0 on success, 1 on failure
-  */
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Description: This program calculates and prints the sum of integers
+ * provided as command-line arguments. If any argument is not a valid integer,
+ * it prints an error message.
+ *
+ * Return: 0 if successful, 1 if there is an error
+ */
 int main(int argc, char **argv)
 {
-		int i, j, sum = 0, check = 1;
+	int i, j, sum = 0, check = 1;
 
-		if (argc == 1)
+	/* Check if there are no arguments */
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		/* Iterate through command-line arguments */
+		for (i = 1; i < argc; i++)
 		{
-			printf("0\n");
+			/* Check if each character in the argument is a digit */
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				{
+					continue;
+				}
+				else
+				{
+					/* If non-digit character found, set check flag to 0 */
+					check = 0;
+					break;
+				}
+			}
+		}
+
+		/* If all arguments are valid integers, calculate their sum */
+		if (check)
+		{
+			for (i = 1; i < argc; i++)
+			{
+				sum += atoi(argv[i]);
+			}
+			printf("%d\n", sum);
 		}
 		else
 		{
-			for (i = 1 ; i < argc ; i++)
-			{
-				for (j = 0 ; argv[i][j] != '\0' ; j++)
-				{
-					if (argv[i][j] >= '0' && argv[i][j] <= '9')
-					{
-						continue;
-					}
-					else
-					{
-						check = 0;
-						break;
-					}
-
-				}
-			}
-
-			if (check)
-			{
-				for (i = 1 ; i < argc ; i++)
-				{
-					sum += atoi(argv[i]);
-				}
-				printf("%d\n", sum);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-
-			}
+			/* If non-integer argument found, print an error message */
+			printf("Error\n");
+			return (1);
 		}
+	}
 
-		return (0);
+	return (0);
 }
-
