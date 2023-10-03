b8503244d70ab120035a21c86e0b9b1f042ed05c
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-03 04:31:07 -0700

    ok

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strlen - returning the lenghe of a string
+  * @s: the stirng which length need to be calculated
+  *
+  * Description: using a counter to calculate each character of the string
+  *
+  * Return: the length of the string
+  */
+
+int _strlen(char *s)
+{
+		int length = 0, i;
+
+		for (i = 0 ; s[i] != '\0' ; i++)
+		{
+			length++;
+		}
+
+		return (length);
+}
