4eac0e2bb643e1c5524a58e555e0c17c0c505455
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-02 18:28:37 -0700

    ok

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,50 +15,50 @@
  */
 int main(int argc, char **argv)
 {
-	int i, j, sum = 0, check = 1;
+		int i, j, sum = 0, check = 1;
 
-	/* Check if there are no arguments */
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		/* Iterate through command-line arguments */
-		for (i = 1; i < argc; i++)
+		/* Check if there are no arguments */
+		if (argc == 1)
 		{
-			/* Check if each character in the argument is a digit */
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (argv[i][j] >= '0' && argv[i][j] <= '9')
-				{
-					continue;
-				}
-				else
-				{
-					/* If non-digit character found, set check flag to 0 */
-					check = 0;
-					break;
-				}
-			}
-		}
-
-		/* If all arguments are valid integers, calculate their sum */
-		if (check)
-		{
-			for (i = 1; i < argc; i++)
-			{
-				sum += atoi(argv[i]);
-			}
-			printf("%d\n", sum);
+			printf("0\n");
 		}
 		else
 		{
-			/* If non-integer argument found, print an error message */
-			printf("Error\n");
-			return (1);
+			/* Iterate through command-line arguments */
+			for (i = 1; i < argc; i++)
+			{
+				/* Check if each character in the argument is a digit */
+				for (j = 0; argv[i][j] != '\0'; j++)
+				{
+					if (argv[i][j] >= '0' && argv[i][j] <= '9')
+					{
+						continue;
+					}
+					else
+					{
+						/* If non-digit character found, set check flag to 0 */
+						check = 0;
+						break;
+					}
+				}
+			}
+	
+			/* If all arguments are valid integers, calculate their sum */
+			if (check)
+			{
+				for (i = 1; i < argc; i++)
+				{
+					sum += atoi(argv[i]);
+				}
+				printf("%d\n", sum);
+			}
+			else
+			{
+				/* If non-integer argument found, print an error message */
+				printf("Error\n");
+				return (1);
+			}
 		}
-	}
 
-	return (0);
+		return (0);
 }
