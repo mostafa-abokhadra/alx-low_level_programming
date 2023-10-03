78ce694a89548137af843d5ab537aa4593d2e21c
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-03 05:26:32 -0700

    ok

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * create_array - dynamic array
+  * @size: size of array
+  * @c: initializing char
+  *
+  * Description: initializing an array with specific character
+  *
+  * Return: pointer to the array
+  */
+char *create_array(unsigned int size, char c)
+{
+		int i;
+
+		char *arr = (char*)malloc(sizeof(char) * size);
+
+		if (size == 0)
+		{
+			return (NULL);
+		}
+		else
+		{
+			if (arr != NULL)
+			{
+				for (i = 0; i < size; i++)
+				{
+					arr[i] = c;
+				}
+			}
+		}
+
+		return (arr);
+}
+
