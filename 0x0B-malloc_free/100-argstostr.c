8946fb51cc98b28fcc1a878a7c5d4d6ef4dfaeda
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-04 13:49:54 -0700

    ok

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,29 +11,33 @@
   */
 char *argstostr(int ac, char **av)
 {
+	int i, sum = 0, j =0,c  = -1;
+
+	char *arr = (char*) malloc(sum * sizeof(char));
+
     if (ac == 0 || av == NULL)
     {
         return NULL;
     }
 
-    int i, sum = 0, j =0,c  = 0;
+ 
 
-    for (int i = 0; i < ac; i++)
+    for (i = 0; i < ac; i++)
     {
         sum += sizeof(av[i]) - sizeof(char);
     }
     sum += 1;
 
-    char *arr = (char*) malloc(sum * sizeof(char));
-
     for(i = 0 ; i < ac ; i ++)
-    {
-        for (j = 0 ; av[i][j] != '\0';j++)
+    { c++;
+        for (j = 0 ; av[i][j] != '\0';j++,c++)
         {
             arr[c] = av[i][j];
-            c++;
+         
         }
+	arr[c] = '\n';
     }
+    arr[c] = '\0';
 
     return arr;
 
