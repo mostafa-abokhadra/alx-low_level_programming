1fda485b007cf636d5a3427bfb5f1c44f526d8b3
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-04 11:54:52 -0700

    ok

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,82 +1,38 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
-* str_l - length of the string
-* @str: the string
-*
-* Description: get the size of a string
-*
-* Return: the size
-*/
-int str_l(char *str)
+  *
+  *
+  *
+  *
+  *
+  *
+  */
+int **alloc_grid(int width, int height)
 {
-int i;
+    int i = 0, j = 0;
 
-if (str == NULL)
-{
+    if (width <= 0 || height <= 0)
+    {
+        return (NULL);
+    }
 
-return (0);
+    int **arr = (int **)malloc(sizeof(int *) * width);
+
+    for (i = 0; i < height; i++)
+    {
+        arr[i] = (int*) malloc(sizeof(int) * width);
+    }
+
+    for (i = 0 ; i < height ; i++)
+    {
+        for (j = 0 ; j < width; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+
+    return arr;
 }
-for (i = 0 ; str[i] != '\0' ; i++)
-{
-}
-return (i);
-
-
-}
-
-/**
-* str_concat - concat 2 strings
-* @s1: first string
-* @s2: second string
-*
-* Description: concating 2 given strings
-* Return: pointer to the new string
-*/
-char *str_concat(char *s1, char *s2)
-{
-int size = 1, j = 0;
-
-char *n_str = (char *)malloc (size * sizeof(char));
-
-size = str_l(s1) + str_l(s2);
-
-
-
-if(n_str != NULL)
-{
-int i;
-
-if (s1 == NULL)
-{
-
-goto fl2;
-}
-
-for(i = 0; s1[i] != '\0' ; i++)
-{
-
-n_str[i] = s1[i];
-}
-
-fl2:
-
-if (s2 == NULL)
-{
-goto end;
-
-}
-
-for (j = 0 ; s2[j] != '\0' && s2 != NULL ; j++ , i++)
-{
-n_str[i] = s2[j];
-}
-end:
-
-n_str[i] = '\0';
-
-}
-
-return (n_str);
-}
-
