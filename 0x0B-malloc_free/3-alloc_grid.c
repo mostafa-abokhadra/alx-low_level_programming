e8831d4b2caa5b8ac65e43ee372229e9bd940261
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-04 17:47:06 -0700

    ok

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		{
 			for (i = 0; i < height ; i++)
 			{
-				arr[i] = (int *)malloc(width *sizeof(int));
+				arr[i] = (int *)malloc(width * sizeof(int));
 				if (arr[i] == NULL)
 				{
 					for (j = i ; j >= 0; j--)
