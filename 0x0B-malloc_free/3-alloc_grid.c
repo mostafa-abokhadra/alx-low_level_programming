8da635281c18d86f1e27a5b6b384da18a05d7d8e
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-04 17:30:19 -0700

    ok

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,38 +11,37 @@
   */
 int **alloc_grid(int width, int height)
 {
-    int i = 0, j = 0, **arr = (int **)malloc(sizeof(int *) * height);
+int i, j, **arr = (int **)malloc(height * sizeof(int *));
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
 
-    if (arr == NULL)
-    { 
-	    for (i = 0 ; i< height; i++)
-	    {
-		    free(arr[i]);
-	    }
-	    free (arr);
-	    return NULL;
-    }
-
-    else
-    {
-    	for (i = 0; i < height; i++)
-    	{
-        	arr[i] = (int*) malloc(sizeof(int) * width);
-    	}
-
-    	for (i = 0 ; i < height ; i++)
-    	{
-        	for (j = 0 ; j < width; j++)
-        	{
-            		arr[i][j] = 0;
-        	}
-    	}
-
-	return (arr);
-    }
+if (width <= 0 || height <= 0)
+{
+return (NULL);
 }
+
+if (arr != NULL)
+{
+for (i = 0; i < height ; i++)
+{
+arr[i] = (int *)malloc(width *sizeof(int));
+}
+
+for (i = 0 ; i < height; i++)
+{
+for (j = 0 ; j < width ; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
+
+}
+else
+{
+free(arr);
+return (NULL);
+}
+
+
+}
+
