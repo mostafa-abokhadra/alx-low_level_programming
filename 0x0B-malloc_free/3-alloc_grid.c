092bafda3a29217f5a69f03f76e4098252464c89
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-04 17:46:09 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,55 +2,50 @@
 #include <stdlib.h>
 
 /**
+  * alloc_grid - dynamic 2d array
+  * @width: the rows
+  * @height: the columns
   *
-  *
-  *
-  *
-  *
-  *
+  * Description: assigning the value 0 to 2d array elements
+  * Return: the 2d array
   */
 int **alloc_grid(int width, int height)
 {
-int i, j, **arr = (int **)malloc(height * sizeof(int *));
+		int i, j, **arr = (int **)malloc(height * sizeof(int *));
 
+		if (width <= 0 || height <= 0)
+		{
+			return (NULL);
+		}
+		if (arr != NULL)
+		{
+			for (i = 0; i < height ; i++)
+			{
+				arr[i] = (int *)malloc(width *sizeof(int));
+				if (arr[i] == NULL)
+				{
+					for (j = i ; j >= 0; j--)
+					{
+						free(arr[j]);
+					}
+					free(arr);
+					return (NULL);
+				}
+			}
 
-if (width <= 0 || height <= 0)
-{
-return (NULL);
-}
-
-if (arr != NULL)
-{
-for (i = 0; i < height ; i++)
-{
-arr[i] = (int *)malloc(width *sizeof(int));
-if (arr[i] == NULL)
-{
-	for (j = i ; j >= 0; j--)
-	{
-		free(arr[j]);
-	}
-	free (arr);
-	return NULL;
-}
-}
-
-for (i = 0 ; i < height; i++)
-{
-for (j = 0 ; j < width ; j++)
-{
-arr[i][j] = 0;
-}
-}
-return (arr);
-
-}
-else
-{
-free(arr);
-return (NULL);
-}
-
-
+			for (i = 0 ; i < height; i++)
+			{
+				for (j = 0 ; j < width ; j++)
+				{
+					arr[i][j] = 0;
+				}
+			}
+			return (arr);
+		}
+		else
+		{
+			free(arr);
+			return (NULL);
+		}
 }
 
