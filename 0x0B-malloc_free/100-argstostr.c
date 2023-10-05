a77ba74e7945bb28942d2b5fd8a4cec115107fce
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-05 08:46:46 -0700

    ok

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,37 +9,67 @@
   *
   *
   */
+int l(int ac, char *av[])
+{
+
+if (ac == 0 || av[0] == NULL)
+{
+return (0);
+}
+
+int i, j, sum = 0;
+
+for (i = 0 ; i < ac; i++)
+{
+if (av[i][0] != '\0')
+{
+for (j = 1; av[i][j] != '\0';j++)
+{
+
+}
+sum += (j + 1);
+}
+}
+return (sum + 1);// +1 for null terminator
+}
+
+/**
+  *
+  *
+  *
+  *
+  *
+  *
+  */
 char *argstostr(int ac, char **av)
 {
-	int i, sum = 0, j =0,c  = -1;
+int size = l(ac,av), i, j, c = 0;
+char *arr = (char *)malloc(size*sizeof(char));
 
-	char *arr = (char*) malloc(sum * sizeof(char));
+if (size == 0)
+{
+arr = NULL;
+}
 
-    if (ac == 0 || av == NULL)
-    {
-        return NULL;
-    }
+if (arr == NULL)
+{
+return NULL;
+}
+else
+{
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0' ; j++, c++)
+{
+arr[c] = av[i][j];
 
- 
+}
+arr[c]='\n';
+c++;
+}
+arr[c]= '\0';
 
-    for (i = 0; i < ac; i++)
-    {
-        sum += sizeof(av[i]) - sizeof(char);
-    }
-    sum += 1;
-
-    for(i = 0 ; i < ac ; i ++)
-    { c++;
-        for (j = 0 ; av[i][j] != '\0';j++,c++)
-        {
-            arr[c] = av[i][j];
-         
-        }
-	arr[c] = '\n';
-    }
-    arr[c] = '\0';
-
-    return arr;
-
+return (arr);
+}
 }
 
