dc03b538eb774f109e2e8bf6e938ba6aa850fa6b
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-07 07:23:01 -0700

    ok

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,13 +7,30 @@
   *
   *
   *
+  */
+
+void _memcpy(char *str, char *str2 , int size)
+{
+    int i;
+
+    for (i = 0 ; str2[i] != '\0' && i < size ; i++)
+    {
+        str[i] = str2[i];
+    }
+}
+/**
+  *
+  *
+  *
+  *
+  *
   *
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
 void *arr;
-int i;
+
 
 if (new_size == 0 && ptr != NULL)
 {
@@ -31,7 +48,7 @@ arr = malloc(new_size);
 else
 {
 arr = malloc(new_size);
-memcpy(arr,ptr,old_size);
+_memcpy(arr,ptr,old_size);
 }
 
 return (arr);
