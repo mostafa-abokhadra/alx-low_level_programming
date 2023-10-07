1c959b5c8ad708c6e0d3a251dd71929782a9ff21
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-07 07:50:42 -0700

    ok

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 
 /**
+  * malloc_checked - creating an array
+  * @b: size of array
   *
-  *
-  *
-  *
-  *
+  * Description: dynamic array creation
+  * Return: the array
   */
 void *malloc_checked(unsigned int b)
 {
-void *arr = malloc(b);
-if (arr == NULL)
-{
-exit(98);
-}
-else
-{
-return arr;
-}
+		void *arr = malloc(b);
+
+		if (arr == NULL)
+		{
+			exit(98);
+		}
+		else
+		{
+			return (arr);
+		}
 }
 
