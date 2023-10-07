96d4fdbeada5cabbd5aa1be54949cd3133b8c5f3
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-07 09:22:28 -0700

    ok

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -52,7 +52,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		if (arr != NULL)
 		{
-			for (i = 0 ; s[i] != '\0'  ; i++)
+			for (i = 0 ; s1[i] != '\0'  ; i++)
 			{
 				arr[i] = s1[i];
 			}
