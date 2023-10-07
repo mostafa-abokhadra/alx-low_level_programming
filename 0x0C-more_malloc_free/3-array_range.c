658a9c3d36b95e4f7ff33a8dabfa4978547f5c50
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-07 09:33:42 -0700

    ok

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,6 +2,47 @@
 #include <stdlib.h>
 
 /**
+  * ll2 - length of array
+  * @min: start point
+  * @max: end point
+  *
+  * Description: getting the length of malloc
+  * Return: the length
+  */
+int ll2(int min, int max)
+{
+		int i, COU = 0;
+
+		for (i = min ; i <= max ; i++)
+		{
+			COU++;
+		}
+		return (COU);
+}
+
+/**
+  *
+  *
+  *
+  *
+  *
+  */
+int check_NULL(int min, int max)
+{
+		int ch;
+
+		if (min > max)
+		{
+			ch = 1;
+		}
+		else
+		{
+			ch = 0;
+		}
+		return (ch);
+}
+
+/**
   *
   *
   *
@@ -9,55 +50,27 @@
   *
   *
   */
-int ll2(int min, int max)
-{
-int i, COU = 0;
-
-for (i = min; i <= max; i++)
-{
-COU++;
-}
-
-return (COU);
-}
-int check_NULL(int min, int max)
-{
-int ch;
-if (min > max)
-{
-ch = 1;
-}
-else
-{
-ch = 0;
-}
-return (ch);
-}
 int *array_range(int min, int max)
 {
-int size = ll2(min, max), *arr = malloc(size * sizeof(int)), i;
+		int size, *arr, i;
 
-if(check_NULL(min, max))
-{
-return (NULL);
-}
-else
-{
-
-if (arr != NULL)
-{
-for (i = 0 ; i < size && min <= max; i++, min++)
-{
-arr[i] = min;
-}
-
-return (arr);
-}
-else
-{
-return (NULL);
-}
-}
-
+		if(check_NULL(min, max))
+		{
+			return (NULL);
+		}
+		size = ll2(min, max);
+		arr = malloc(size * sizeof(int));
+		if (arr != NULL)
+		{
+			for (i = 0 ; i < size ; i++, min++)
+			{
+				arr[i] = min;
+			}
+			return (arr);
+		}
+		else
+		{
+			return (NULL);
+		}
 }
 
