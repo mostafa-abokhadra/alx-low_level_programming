3a60eaccb91c068907f16b9dbe20d9ceff347b3c
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-07 09:39:37 -0700

    ok

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,11 +21,12 @@ int ll2(int min, int max)
 }
 
 /**
+  * check_NULL - checking NULL
+  * @min: if greater is NULL
+  * @max: if greater not NULL
   *
-  *
-  *
-  *
-  *
+  * Description: check if we will return NULL
+  * Return: 1 if min greater than max, 0 if not
   */
 int check_NULL(int min, int max)
 {
@@ -43,18 +44,18 @@ int check_NULL(int min, int max)
 }
 
 /**
+  * array_range - inserting a range in an array
+  * @min: start point
+  * @max: end point
   *
-  *
-  *
-  *
-  *
-  *
+  * Description: from min to max in an array
+  * Return: the arrayd
   */
 int *array_range(int min, int max)
 {
 		int size, *arr, i;
 
-		if(check_NULL(min, max))
+		if (check_NULL(min, max))
 		{
 			return (NULL);
 		}
