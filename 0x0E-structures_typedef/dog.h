7585f0d8c76da930fd90f91c8ecbf4a4e660e01d
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-10 14:09:12 -0700

    ok

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+  * struct dog - new type
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  *
+  * Description: making new type
+  */
+struct dog
+{
+		char *name;
+		float age;
+		char *owner;
+};
+
