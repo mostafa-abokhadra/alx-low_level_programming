3d7f01496805f2aa8348801981aea79510120781
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-10 16:25:26 -0700

    ok

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * free_dog - free a memmory
+  * @d: the memmory to be freed
+  *
+  * Description: deleting a dynamic memmory allocation
+  *
+  * Return: nothing
+  */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
