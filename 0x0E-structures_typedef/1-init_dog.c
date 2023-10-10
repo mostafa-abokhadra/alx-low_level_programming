2041b4e33aee487d53e6ae68b8fe8856851b6815
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-10 16:31:29 -0700

    ok

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
   * init_dog - initializing a dog type
@@ -14,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		if (d == NULL)
 		{
 			free(d);
-			return (NULL);
+			return;
 		}
 		d->name = name;
 		d->age = age;
