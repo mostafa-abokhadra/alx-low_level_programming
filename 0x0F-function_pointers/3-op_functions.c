f4b99ec5c72ba2646723c65161b1ba1c02b7a476
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-11 15:28:41 -0700

    ok

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,3 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
   * op_add - sum of 2 nums
   * @a: first num
@@ -51,11 +47,6 @@ int op_mul(int a, int b)
   */
 int op_div(int a, int b)
 {
-		if (b == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
 		return (a / b);
 }
 
@@ -69,11 +60,6 @@ int op_div(int a, int b)
   */
 int op_mod(int a, int b)
 {
-		if (b == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
 		return (a % b);
 }
 
