83cd76e9895d8b2bfa8dfb67c5d48ed0fb3c770e
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-10 17:47:11 -0700

    ok

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: print the file name
+  *
+  * Return: always zero
+  */
+int main()
+{
+		printf("%s\n", __FILE__);
+		return (0);
+}
