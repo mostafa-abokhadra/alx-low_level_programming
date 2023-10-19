9c1b210a493930f7aa7deec2bc23a8cc353147ae
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-19 14:02:31 -0700

    header edition

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef __LIST_H__
-#define __LIST_H_
+#ifndef __HEADER__
+#define __HEADER__
 
 /**
  * struct list_s - singly linked list
@@ -11,9 +11,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+		char *str;
+		unsigned int len;
+		struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -22,4 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif /* __LIST_H__  */
+
+#endif /* __HEADER__  */
