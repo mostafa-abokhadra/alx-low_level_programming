a84b93170ad11d8a57f12e1445defd50f2947a12
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-19 18:52:21 -0700

    ok

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,43 +3,45 @@
 #include <string.h>
 
 /**
+  * add_node_end - adding node to tail
+  * @head: node head
+  * @str: string to be added
   *
-  *
-  *
-  *
-  *
+  * Description: add to end of linked list
+  * Return: address of new added node
   */
-list_t* add_node_end(list_t** head, const char* str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-list_t* new_node = (list_t*)malloc(sizeof(list_t)), *t = *head;
+		list_t *new_node = (list_t *)malloc(sizeof(list_t)), *t = *head;
 
-if (new_node == NULL)
-return (NULL);
-if (str == NULL)
-new_node->str = NULL;
-else
-{
-new_node->str = (char*)malloc(sizeof(char) * strlen(str) + 1);
-if (new_node->str == NULL)
-{
-free(new_node->str);
-free(new_node);
-return (NULL);
+		if (new_node == NULL)
+			return (NULL);
+		if (str == NULL)
+			new_node->str = NULL;
+		else
+		{
+			new_node->str = (char *)malloc(sizeof(char) * strlen(str) + 1);
+			if (new_node->str == NULL)
+			{
+				free(new_node->str);
+				free(new_node);
+				return (NULL);
+			}
+			new_node->str = strdup(str);
+			new_node->next = NULL;
+			new_node->len = strlen(str);
+		}
+		if (*head == NULL)
+		{
+			*head = new_node;
+			return (*head);
+		}
+		else
+		{
+			while (t->next != NULL)
+				t = t->next;
+			t->next = new_node;
+			return (t->next);
+		}
 }
-new_node->str = strdup(str);
-new_node->next = NULL;
-new_node->len = strlen(str);
-}
-if (*head == NULL)
-{
-*head = new_node;
-return (*head);
-}
-else
-{
-while (t->next != NULL)
-t = t->next;
-t->next = new_node;
-return (t->next);
-}
-}
+
