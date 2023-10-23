51c4e6216fe2fd27fee2261c034a90d273e5ee21
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-23 14:03:08 -0700

    ok

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,11 @@ void free_listint2(listint_t **head)
 {
 		listint_t *temp;
 
-		if (*head == NULL)
-			return;
 		while ((*head) != NULL)
 		{
-			temp = (*head);
-			(*head) = (*head)->next;
-			free(temp);
+			temp = (*head)->next;
+			free(*head);
+			(*head) = temp;
 		}
 		(*head) = (NULL);
 }
