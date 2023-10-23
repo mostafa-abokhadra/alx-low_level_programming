226293c5185fef3508feacba6e7cfcbc37ea7002
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-23 13:31:01 -0700

    doc

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+  * listint_len - length of a linked list
+  * @h: header
+  *
+  * Description: length or size
+  * Return: the length
+  */
 size_t listint_len(const listint_t *h)
 {
 		unsigned int number_of_nodes = 0;
