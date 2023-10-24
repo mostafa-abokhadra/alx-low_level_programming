8b2574e052022ad8165004bddf3914e3c6946d00
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-24 15:34:33 -0700

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		listint_t *temp_store, *temp_head = *head;
 
 		if (*head == NULL)
-			return (- 1);
+			return (-1);
 		else if (*head != NULL && index == 0)
 		{
 			temp_store = *head;
@@ -38,7 +38,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 				return (1);
 			}
 			else
-				return (1);
+				return (-1);
 		}
 }
 
