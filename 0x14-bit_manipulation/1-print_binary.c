a5cc682bf00a69c826a78b1f7818cc55e66bdfc9
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-26 16:58:49 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *
+  *
+  *
+  *
+  */
+void print_binary(unsigned long int n)
+{
+unsigned int rem = 0;
+
+if (n == 0 || n == 1)
+{
+_putchar(n + '0');
+return;
+}
+if (n == 0)
+return;
+rem = n % 2;
+print_binary(n/2);
+_putchar(rem + '0');
+}
+
