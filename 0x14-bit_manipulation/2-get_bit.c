7536bd7a7444dedff364c9a6c532da00361d310b
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-27 11:14:36 -0700

    ok

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-  * get_bit - getting bit
-  * @n: number
-  * @index: idx
-  *
-  * Description: right sifting and and
-  * Return: result
-  */
-int get_bit(unsigned long int n, unsigned int index)
+ * set_bit - sets the bit at the index
+ * @n: the number to index
+ * @index: the bit to set
+ *
+ * Return: 1 if successful, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index >= sizeof(n) * 8)
-                return (-1);
+	if (index >= sizeof(n) * 8)
+		return (-1);
 
-        return (n >> index & 1);
+	return (!!(*n |= 1L << index));
 }
-
-
--- 0x14-bit_manipulation/2-set_bit.c
+++ 0x14-bit_manipulation/2-set_bit.c
@@ -9,8 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-		return (-1);
-
-	return (!!(*n |= 1L << index));
+		if (index >= sizeof(n) * 8)
+			return (-1);
+		return (!!(*n |= 1L << index));
 }
