ef0f4bfb8c50ded66e401a0544fa79c2685f2216
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-27 10:25:43 -0700

    my code worked:)

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,41 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
-/**
-  *
-  *
-  *
-  *
-  *
-  */
-int _strlen(const char *str)
-{
-unsigned int i;
-
-for (i = 0; str[i] != '\0'; i++)
-{
-}
-return (i);
-}
 
 /**
+  * get_binary - converting int to binary
+  * @n: number
   *
-  *
-  *
-  *
+  * Description: conv to int
+  * Return: binary num
   */
 char *get_binary(unsigned long int n)
 {
-unsigned size = sizeof(n) * 8;
-    char *bin_str = malloc(sizeof(char) * size + 1);
-    unsigned int i;
+		unsigned int size = sizeof(n) * 8, i;
+		char *bin_str = malloc(sizeof(char) * size + 1);
 
-    for (i = 0; n; i++)
-    {
-        bin_str[i] = ((n % 2) + '0');
-        n /= 2;
-    }
-    bin_str[i] = '\0';
-    return (bin_str);
+		for (i = 0; n; i++)
+		{
+			bin_str[i] = ((n % 2) + '0');
+			n /= 2;
+		}
+		bin_str[i] = '\0';
+		return (bin_str);
 }
 /**
  * get_bit - gets the bit at the index
@@ -43,22 +27,13 @@ unsigned size = sizeof(n) * 8;
  * @index: the bit to get
  *
  * Return: the bit state or -1 on error
- *
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-		return (-1);
+		char *str = get_binary(n);
 
-	return (n >> index & 1);
-}
-*/
-int get_bit(unsigned long int n, unsigned int index)
-{
-char *str = get_binary(n);
-
-if(index >= sizeof(n) * 8)
-	return -1;
-return (str[index] - '0');
-
+		if (index >= sizeof(n) * 8)
+			return (-1);
+		return (str[index] - '0');
 }
 
