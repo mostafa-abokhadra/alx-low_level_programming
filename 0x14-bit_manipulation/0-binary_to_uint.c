ceb1e198e86b7f735a4e4141ecd893f8d967f2b7
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-26 17:25:00 -0700

    ok

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,60 +2,57 @@
 #include <stdlib.h>
 
 /**
+  * _strlen - str length
+  * @b: the string
   *
-  *
-  *
-  *
-  *
+  * Description: length of a str
+  * Return: length
   */
 int _strlen(const char *b)
 {
-int i;
+		int i;
 
-for (i = 0 ; b[i] != '\0' ; i++)
-{
-
-}
-
-return (i);
+		for (i = 0 ; b[i] != '\0' ; i++)
+		{
+		}
+		return (i);
 }
 /**
+  * binary_to_uint - convert to int
+  * @b: binary num
   *
-  *
-  *
-  *
-  *
+  * Description: converting to a decimal
+  * Return: converted num
   */
 unsigned int binary_to_uint(const char *b)
 {
-char *rev;
-unsigned int i, j, num = 1;
-unsigned int dec = 0;
+		char *rev;
+		unsigned int i, j, num = 1, dec = 0;
 
-if (b == NULL)
-return (0);
-j = _strlen(b);
-rev = malloc(sizeof(char) *  j + 1);
-for (i = 0 ; b[i] != '\0' ; i++)
-rev[i] = b[--j];
-rev[i]='\0';
-for (i = 0; rev[i] != '\0'; i++)
-{
-if (rev[i] == '1' || rev[i] == '0')
-{
-if (i == 0)
-dec += (rev[i] - '0') * 1;
-else
-{
-for (j = 1 ; j <= i ; j++)
-num *= 2;
-dec += (rev[i] - '0') * num;
-num = 1;
-}
-}
-else
-return(0);
-}
-return (dec);
+		if (b == NULL)
+			return (0);
+		j = _strlen(b);
+		rev = malloc(sizeof(char) *  j + 1);
+		for (i = 0 ; b[i] != '\0' ; i++)
+			rev[i] = b[--j];
+		rev[i] = '\0';
+		for (i = 0; rev[i] != '\0'; i++)
+		{
+			if (rev[i] == '1' || rev[i] == '0')
+			{
+				if (i == 0)
+					dec += (rev[i] - '0') * 1;
+				else
+				{
+					for (j = 1 ; j <= i ; j++)
+						num *= 2;
+					dec += (rev[i] - '0') * num;
+					num = 1;
+				}
+			}
+			else
+				return (0);
+		}
+		return (dec);
 }
 
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,61 +2,87 @@
 #include <stdlib.h>
 
 /**
+  * _strlen - length
+  * @b: string
   *
-  *
-  *
-  *
+  * Description: length of string
+  * Return: the length
   */
 int _strlen(const char *b)
 {
-int i;
+		int i;
 
-for (i = 0 ; b[i] != '\0' ; i++)
-{
+		for (i = 0 ; b[i] != '\0' ; i++)
+		{
+		}
+		return (i);
+}
 
-}
-return (i);
-}
-char *rev_str (char *str)
+/**
+  * rev_str - rev a str
+  * @str: the string
+  *
+  * Description: reversing string
+  * Return: reversed string
+  */
+char *rev_str(char *str)
 {
-int j = _strlen(str), i;
-char *str2 = malloc(sizeof(char) * j + 1);
-for (i = 0; str[i] != '\0'; i++)
-{
-str2[i] = str[--j];
+		int j = _strlen(str), i;
+		char *str2 = malloc(sizeof(char) * j + 1);
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			str2[i] = str[--j];
+		}
+		str2[i] = '\0';
+		return (str2);
 }
-str2[i]='\0';
-return (str2);
-}
+
+/**
+  * conv_to_binary - converting to binary
+  * @n: the number
+  *
+  * Description: conv to bin
+  * Return: converted num
+  */
 char *conv_to_binary(unsigned long int n)
 {
-char *conv = malloc(sizeof(char) * 2);
-int i = 0, re = 1;
+		char *conv = malloc(sizeof(char) * 2);
+		int i = 0, re = 1;
 
-if (n == 0 || n == 1)
-{
-conv[i++] = n +  '0';
+		if (n == 0 || n == 1)
+		{
+			conv[i++] = n +  '0';
+		}
+		else
+		{
+			for (i = 0 ; n ; i++)
+			{
+				conv = realloc(conv, sizeof(char) * (++re));
+				conv[i] = (n % 2 + '0');
+				n /= 2;
+			}
+		}
+		conv[i] = '\0';
+		return (rev_str(conv));
 }
-else
-{
-for (i = 0 ; n ; i++)
-{
-conv = realloc(conv, sizeof(char) * (++re));
-conv[i] = (n % 2 + '0');
-n /= 2;
-}
-}
-conv[i] = '\0';
-return (rev_str(conv));
-}
+
+/**
+  * get_bit - gitting bit
+  * @n: number
+  * @index: index
+  *
+  * Description: getting bit in an index
+  * Return: 1 if success, -1 if not
+  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-char *str_bin = conv_to_binary(n);
-unsigned int size = _strlen(str_bin);
+		char *str_bin = conv_to_binary(n);
+		unsigned int size = _strlen(str_bin);
 
-if (index >= size)
-return (-1);
-else
-return (str_bin[index] - '0');
+		if (index >= size)
+			return (-1);
+		else
+			return (str_bin[index] - '0');
 }
 
