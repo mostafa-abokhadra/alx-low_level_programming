8dc06a1f7987367aa58b78358cb9260378083024
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-30 06:15:05 -0700

    ok

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+  * _len - string length
+  * @str: string
+  *
+  * Description: array length of char
+  * Return: the length
+  */
+int _len(char *str)
+{
+		int counter = 0;
+
+		while (str[counter] != '\0')
+		{
+			counter++;
+		}
+		return (counter);
+}
+
+/**
+  * appent_text_to_file - append
+  * @filename: file name
+  * @text_content: txt
+  *
+  * Description: appending text
+  * Return: 1 or -1
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+		int fd;
+
+		fd = open(filename, O_APPEND);
+		if (fd < 0 || filename == NULL)
+			return (-1);
+		write(fd, text_content, _len(text_content));
+		if (text_content == NULL && fd >= 0)
+			return (1);
+		else if (text_content == NULL && fd < 0)
+			return (-1);
+		return (1);
+}
