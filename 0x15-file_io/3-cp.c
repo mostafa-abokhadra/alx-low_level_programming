f883dcafc1447609f5e57b299e8023422f09b264
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-30 19:25:38 -0700

    ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,6 +8,15 @@
 #define REA "Error: Can't read from file %s\n"
 #define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 #define WRITING "Error: Can't write to %s\n"
+
+/**
+  * main - entry point
+  * @count: arg count
+  * @av: list of args
+  *
+  * Description: copying a file into a new file
+  * Return: success
+  */
 int main(int count, char **av)
 {
 		int fd1, fd2, r_c;
@@ -18,10 +27,10 @@ int main(int count, char **av)
 		fd1 = open(av[1], O_RDONLY);
 		if (fd1 < 0)
 			dprintf(STDERR_FILENO, REA, av[1]), exit(98);
-		fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERM );
-		if(fd2 < 0)
+		fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERM);
+		if (fd2 < 0)
 			dprintf(STDERR_FILENO, WRITING, av[2]), exit(99);
-		while((r_c = read(fd1, c, 1024)) > 0)
+		while ((r_c = read(fd1, c, 1024)) > 0)
 			if (write(fd2, c, r_c) != r_c)
 				dprintf(STDERR_FILENO, WRITING, av[2]), exit(99);
 		if (r_c < 0)
