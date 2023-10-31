0dc19109cec91485d5ebc00f22fd6cb24bed0771
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-30 19:23:17 -0700

    ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,48 +3,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define CLOSE "Error: Can't close fd %d\n"
+#define USAGE "Usage: cp file_from file_to\n"
+#define REA "Error: Can't read from file %s\n"
+#define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define WRITING "Error: Can't write to %s\n"
 int main(int count, char **av)
 {
-		int fd1, fd2, r_c, w_r, co, co2;
+		int fd1, fd2, r_c;
 		char *c = malloc(sizeof(char) * 1024);
 
 		if (count != 3)
-		{
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit(97);
-		}
+			dprintf(STDERR_FILENO, USAGE), exit(97);
 		fd1 = open(av[1], O_RDONLY);
-		fd2 = open(av[2], O_WRONLY | O_TRUNC | O_APPEND);
 		if (fd1 < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",av[1]);
-			exit(98);
-		}
-		while((r_c = read(fd1, c, 1024)) != 0)
-		{
-			if (r_c < 0)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",av[1]);
-				exit(98);
-			}
-			w_r = write(fd2, c, 1);
-			if (w_r < 0)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-				exit(99);
-			}
-		}
-		co = close(fd1);
-		co2 = close(fd2);
-		if (co != 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-			exit(100);
-		}
-		else if (co2 != 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-			exit(100);
-		}
-		return (0);
+			dprintf(STDERR_FILENO, REA, av[1]), exit(98);
+		fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERM );
+		if(fd2 < 0)
+			dprintf(STDERR_FILENO, WRITING, av[2]), exit(99);
+		while((r_c = read(fd1, c, 1024)) > 0)
+			if (write(fd2, c, r_c) != r_c)
+				dprintf(STDERR_FILENO, WRITING, av[2]), exit(99);
+		if (r_c < 0)
+			dprintf(STDERR_FILENO, REA, av[1]), exit(98);
+		fd1 = close(fd1);
+		fd2 = close(fd2);
+		if (fd1)
+			dprintf(STDERR_FILENO, CLOSE, fd1), exit(100);
+		else if (fd2)
+			dprintf(STDERR_FILENO, CLOSE, fd2), exit(100);
+		return (EXIT_SUCCESS);
 }
