45cf4206dc33a11f3db8e41d3336441e6b193570
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-30 18:04:16 -0700

    ok

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 		if (!filename)
 			return (-1);
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-		if(fd < 0)
+		if (fd < 0)
 			return (-1);
 		if (text_content == NULL)
 			return (1);
