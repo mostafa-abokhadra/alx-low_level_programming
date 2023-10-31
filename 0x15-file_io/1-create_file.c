d6667237dc19367549e031476b9cac05be294685
Mostafa Bukhadra <mvxizk@gmail.com>
2023-10-30 17:56:49 -0700

    ok

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int _len(char *str)
 {
 		int counter = 0;
 
+		if (!str)
+			return (0);
 		while (str[counter] != '\0')
 		{
 			counter++;
@@ -33,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 
 		if (!filename)
 			return (-1);
-		fd = open(filename, O_CREAT | O_TRUNC, 500);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR, 500);
 		if(fd < 0)
 			return (-1);
 		else
@@ -44,5 +46,7 @@ int create_file(const char *filename, char *text_content)
 			if (count < 0)
 				return (-1);
 		}
-		return (1);
+		if (count == _len(text_content))
+			return (1);
+		return (-1);
 }
