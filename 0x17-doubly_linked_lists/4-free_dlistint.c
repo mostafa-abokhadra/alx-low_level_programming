57c44366d96898733e44ceca6b0ac8e6eae24262
mostafa bukhadra <mvxizk@gmail.com>
2023-12-07 20:40:34 +0200

    ok

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * free_dlistint - freeing
+ * @head: head of list
+ *
+ * Description: freeing heap
+ * Return: nothing
+ */
 void free_dlistint(dlistint_t *head)
 {
 		dlistint_t *t = head;
 
-		for (;t != NULL;)
+		for (; t != NULL ;)
 		{
 			head = head->next;
 			free(t);
