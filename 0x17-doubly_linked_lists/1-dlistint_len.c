23f035e55b48381319fd7346eb820ac992962a25
mostafa bukhadra <mvxizk@gmail.com>
2023-12-07 19:59:33 +0200

    ok

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * dlistint_len - lenght
+ * @h: head of list
+ *
+ * Description: getting the lenght
+ * Return: the lenght
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 		const dlistint_t *t = h;
@@ -7,6 +14,6 @@ size_t dlistint_len(const dlistint_t *h)
 
 		if (!h)
 			return (0);
-		for (size = 0; t != NULL; t = t-> next, size++){}
+		for (size = 0; t != NULL; t = t->next, size++){}
 		return (size);
 }
