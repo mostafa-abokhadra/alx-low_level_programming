fe65313c475887a642f020f778be058d1fc8cdb9
mostafa bukhadra <mvxizk@gmail.com>
2023-12-08 01:48:15 +0200

    ok

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+		
+	/*
 		unsigned int step = 0;
 		dlistint_t *t = *h, *new_node = NULL;
 
@@ -36,4 +38,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			free(new_node);
 			return (NULL);
 		}
+	*/
 }
