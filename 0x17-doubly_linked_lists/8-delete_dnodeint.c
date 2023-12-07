a84c9928b1748b7e2e10c448093e26d96314b633
mostafa bukhadra <mvxizk@gmail.com>
2023-12-08 00:20:34 +0200

    ok

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,7 +7,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		unsigned int i = 0;
 
 		if (!(*head))
-			return (-1);
+			return (1);
 		if (index == 0)
 		{
 			*head = (*head)->next;
