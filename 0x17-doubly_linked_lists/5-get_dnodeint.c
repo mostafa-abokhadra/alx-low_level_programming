f1c61ae1ff14373f9823cbfb32ccadecf8bd4e67
mostafa bukhadra <mvxizk@gmail.com>
2023-12-07 20:48:07 +0200

    ok

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - getting node
+ * @head: head of list
+ * @index: index of wanted node
+ *
+ * Description: getting a node
+ * Return: address of wanted node
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 		unsigned int pos = 0;
