782c7c273e96a995b31f670eab4de3b4d95d465e
mostafa bukhadra <mvxizk@gmail.com>
2023-12-08 03:16:32 +0200

    ok

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,57 +13,30 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 		dlistint_t *new_mem, *res_mem = *h;
-	/* unsigned int counter = 0 */
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
-	{
-		if (res_mem == NULL)
-			return (NULL);
-		res_mem = res_mem->next;
-	}
-
-	if (res_mem->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	new_mem = malloc(sizeof(dlistint_t));
-
-	if (new_mem == NULL)
-		return (NULL);
-
-	new_mem->n = n;
-	new_mem->next = res_mem->next;
-	new_mem->prev = res_mem;
-	res_mem->next->prev = new_mem;
-	res_mem->next = new_mem;
-
-	return (new_mem);
-	/*
-		unsigned int step = 0;
-		dlistint_t *t = *h, *new_node = NULL;
 
 		if (idx == 0)
 			return (add_dnodeint(h, n));
-		new_node = malloc(sizeof(dlistint_t));
-		if (!new_node)
-			return (NULL);
-		new_node->n = n;
-		for (; step < idx - 1 && t->next != NULL; t = t->next, step++)
-		{}
-		if (step == idx - 1)
+
+		for (; idx != 1; idx--)
 		{
-			new_node->next = t->next;
-			t->next = new_node;
-			new_node->prev = t;
-			new_node->next->prev = new_node;
-			return (new_node);
+			if (res_mem == NULL)
+				return (NULL);
+			res_mem = res_mem->next;
 		}
-		else
-		{
-			free(new_node);
+
+		if (res_mem->next == NULL)
+			return (add_dnodeint_end(h, n));
+
+		new_mem = malloc(sizeof(dlistint_t));
+
+		if (new_mem == NULL)
 			return (NULL);
-		}
-	*/
+
+		new_mem->n = n;
+		new_mem->next = res_mem->next;
+		new_mem->prev = res_mem;
+		res_mem->next->prev = new_mem;
+		res_mem->next = new_mem;
+
+		return (new_mem);
 }
