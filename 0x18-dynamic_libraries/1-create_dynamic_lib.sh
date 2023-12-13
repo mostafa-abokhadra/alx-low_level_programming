70818a68f12db77efffd251abc57adcbd19e2eea
mostafa bukhadra <mvxizk@gmail.com>
2023-12-13 02:58:05 +0200

    ok

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -pedantic -Werror -fPIC -c *.c
+gcc -Wall -Wextra -pedantic -Werror -c -fPIC *.c
 gcc -shared *.o -o liball.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
