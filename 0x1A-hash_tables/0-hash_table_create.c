9dbbc5653c2e4ee7d6a5c6e4483863780eea1f0d
mostafa bukhadra <mvxizk@gmail.com>
2023-12-23 04:09:38 +0200

    ok

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,11 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 		if (!table)
 			return (NULL);
-		table->array = malloc(sizeof(hash_node_t *) * size + 1);
+		table->array = malloc(sizeof(hash_node_t *) * size);
 		if (!table->array)
 			return (NULL);
 		table->size = size;
-		for (i = 0; i < size + 1; i++)
+		for (i = 0; i < size ; i++)
 			table->array[i] = NULL;
 		return (table);
 
