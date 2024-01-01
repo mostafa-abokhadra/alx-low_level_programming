9ffe8ab07059f411a724883e2cc0112d6ff2aad8
mostafa bukhadra <mvxizk@gmail.com>
2024-01-01 19:31:22 +0200

    ok

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,24 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: the size, in number of nodes, to make the new hash table.
+ * hash_table_create - creating a table
+ * @size: size of table
  *
- * Return: a pointer to the newly created hash table.
+ * Description: just a hash table
+ * Return: the hesh table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	hash_table_t *hashtable;
+		hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (size == 0)
-		return (NULL);
-	hashtable = malloc(sizeof(hash_table_t));
-	if (hashtable == NULL)
-		return (NULL);
-	hashtable->size = size;
-	hashtable->array = malloc(sizeof(hash_node_t *) * size);
-	if (hashtable->array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		hashtable->array[i] = NULL;
-	}
-	return (hashtable);
+		if (!table)
+			return (NULL);
+		table->size = size;
+		table->array = malloc(sizeof(hash_node_t) * size + 1);
+		if (!table->array)
+		{
+			free(table);
+			return (NULL);
+		}
+		return (table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -34,22 +34,12 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-
 unsigned long int hash_djb2(const unsigned char *str);
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
-hash_node_t *set_pair(const char *key, const char *value);
-
-int set_pair_only(hash_table_t *ht, const char *key, const char *value,
-		  unsigned long int size);
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
 void hash_table_print(const hash_table_t *ht);
-
 void hash_table_delete(hash_table_t *ht);
+
 
 #endif
