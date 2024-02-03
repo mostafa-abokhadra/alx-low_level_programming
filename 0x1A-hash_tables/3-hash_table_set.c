643ba40762c8f96a5c9525e9a6b131e63bbf23cd
mostafa bukhadra <mvxizk@gmail.com>
2024-02-03 03:13:15 +0200

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,13 +37,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			new_node->next = NULL;
 			ht->array[idx] = new_node;
-			return (1);
 		}
 		else
 		{
 			new_node->next = ht->array[idx];
 			ht->array[idx] = new_node;
-			return (1);
 		}
-		return (0);
+		return (1);
 }
